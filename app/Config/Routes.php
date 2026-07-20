ba51258e2fdbc759946c53f793969d499d3ce3c9
nofyrabe777 <nofyrabe@gmail.com>
2026-07-20 11:07:55 +0300

    Réorganiser les fichiers ouverts récemment et mettre à jour les routes pour les transactions

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,10 +1,19 @@
 <?php
 
 use CodeIgniter\Router\RouteCollection;
-
+use App\Controllers\TransactionsController;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
-$routes->get('/produit','Produit::index');
-$routes->get('/produit/(:num)', 'Produit::show/$1');
+// $routes->get('/', 'Home::index');
+
+$routes->group('transaction', function ($routes) {
+    // Affichage du solde et de l'historique d'un compte
+    // Exemple d'URL : GET /transaction/compte/1
+    $routes->get('compte/(:num)', 'TransactionsController::index/$1');
+
+    // Effectuer les opérations (méthode POST)
+    $routes->post('depot', 'TransactionsController::depot');
+    $routes->post('retrait', 'TransactionsController::retrait');
+    $routes->post('transfert', 'TransactionsController::transfert');
+});
